3265450d1d0e0dc2835a18be785f74d4a3c45638
غياث <ajamghiath@gmail.com>
2021-05-24 07:29:32 +0200

    lec.8 + minor changes
    improvements mostly inspired by lecture 8 + some changes in decision making, algorithm still the same

--- Project.ino
+++ Project.ino
@@ -1,20 +1,25 @@
 #include "HX711.h"
 
+// this code assumes readings are in grams
+
 // being falling from chair alert
 HX711 hx;
 
-#define  calibration_factor         600       // This value is obtained using the SparkFun_HX711_Calibration sketch
-#define  HX711_DOUT_PIN             4         // dout pin
-#define  HX711_SCK_PIN              3         // sck pin
-#define  NUM_OF_READINGS            20        // used to average on readings
-#define  NUM_STORED_READINGS        5         // size of the readings array
-#define  MAX_ACCEPTABLE_WEIGHT      5         // if weight is exceeds this -> OVERLOAD!
-const double Weight_Threshold = 10; // maximum acceptable [alert-less] rate of change of weight with respect to time
+#define calibration_factor        600       // This value is obtained using the SparkFun_HX711_Calibration sketch
+#define HX711_DOUT_PIN            4         // dout pin
+#define HX711_SCK_PIN             3         // sck pin
+#define NUM_OF_READINGS           20        // used to average on readings
+#define NUM_STORED_READINGS       5         // size of the readings array
+#define MAX_ACCEPTABLE_WEIGHT     6000      // if weight is exceeds this -> OVERLOAD!
+#define button                    2         // button pin
+#define buzzer                    11        // buzzer pin
+#define BUZZ_FREQ                 500       // signal sent to buzzer via function tone()
+// maximum acceptable [alert-less] rate of DECREASE change of weight with respect to time ->
+//( diff. in Weight / diff. in Time ) == gram per second
+#define Weight_Threshold          500
 
-const int button = 2;
-const int buzzer = 11;
-
-volatile bool buzzer_ringing = 0;
+// volatile for interrupt 
+volatile byte buzzer_ringing = 0;
 
 struct HXPAIR 
 {
@@ -23,8 +28,7 @@ struct HXPAIR
     volatile double w_reading = 0;
 };
 
-struct HXPAIR readings[NUM_OF_READINGS];
-// end falling from chair alert
+struct HXPAIR readings[NUM_STORED_READINGS];
 
 void setup()
 {
@@ -35,6 +39,7 @@ void setup()
     pinMode(buzzer,  OUTPUT);
     buzzer_ringing = LOW;
 
+    // attach button pin to interrupt on rising event
     attachInterrupt(digitalPinToInterrupt(button),resett,RISING);
     
     // begin falling from chair alert
@@ -44,8 +49,7 @@ void setup()
     hx.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
     
     for (int i = 0; i < NUM_STORED_READINGS; i++)
-        readings[i].t_reading = 0;// time zero = time for this i   was not set yet
-    //end falling from chair alert
+        readings[i].t_reading = 0;// time zero --> time for this i was not set yet
 }
 
 void read_and_store()
@@ -79,6 +83,7 @@ void read_and_store()
     }
 
     // store the most recent reading
+    // store time in MILLI seconds
     readings[0].t_reading = curr_time;
     readings[0].w_reading = curr_value;
 }
@@ -86,17 +91,18 @@ void read_and_store()
 void resett()
 {
    //shut-down if button pressed
-  if(buzzer_ringing==LOW)
+   if(buzzer_ringing == LOW)
      {
-        Serial.println("button is pressed, buzzer already off, no action");
+        Serial.println(F("button is pressed, buzzer already off, no action"));
         return;
      }
 
-   Serial.println("button is pressed, turning off buzzer");
+   Serial.println(F("button is pressed, turning off buzzer  "));
    noTone(buzzer);
    buzzer_ringing = LOW;
-   //clearing the array as we are selecting multiple readings,
-   //we dont want same reading to induce alarm more than once
+   
+   //clearing the array, as we are selecting multiple readings -> we dont want ->
+   //same reading to induce alarm more than once
    for (int i = 0; i < NUM_STORED_READINGS; i++)
         readings[i].t_reading = 0;// time zero = time for this i   was not recorded yet
 
@@ -106,34 +112,43 @@ void loop()
 { 
   // TEST BLOCK //////////////////////////////////
     delay(70);
-    Serial.println("ABBBBBAAA");
-    Serial.print("Button is: ");
+    Serial.print(F("Button is: "));
     Serial.println(digitalRead(button));
 
-    Serial.print("Buzzer is: ");
+    Serial.print(F("Buzzer is: "));
     Serial.println(buzzer_ringing);
 
   ///////////////////////////////////////////////
   
-  //return as we don't care what happens
+  //if already ringing, return as we don't care what happens
   if(buzzer_ringing)
    {
-       Serial.println("ringing....");      
+       Serial.println(F("ringing...."));      
        return;
    }
    
   //get current reading
   read_and_store();
- 
-  Serial.print("Current readings: ");
+
+  // OVERLOAD check
+  if( readings[0].w_reading > MAX_ACCEPTABLE_WEIGHT )
+  {
+    Serial.print(F("OVERLOAD! "));
+    buzzer_ringing=HIGH;
+    tone (buzzer,BUZZ_FREQ);  
+    return;
+  }
+
+  // display readings array
+  Serial.print( F("Current readings: ") );
   for (int i = 0; i < NUM_STORED_READINGS; i++)
       {
          Serial.print(readings[i].w_reading);
-         Serial.print(" ");
+         Serial.print("   ");
       }
   Serial.println();
   
-  Serial.print("Current TIME readings: ");
+  Serial.print( F("Current TIME readings: ") );
   for (int i = 0; i < NUM_STORED_READINGS; i++)
       {
          Serial.print(readings[i].t_reading);
@@ -148,43 +163,51 @@ void loop()
         return;
       }*/
       
-  //if no readings yet, theoritcially impossible but meh
-  if(readings[0].t_reading==0)
+  //if no readings yet retutn, theoritcially impossible but meh
+  if( readings[0].t_reading == 0 )
       {
-        Serial.println("no readings yet ");
+        Serial.println( F("no readings yet") );
         return;
       }
       
   //Get the maximum "speed" over previous readings
   double speed = 0;
-  for(int i =0;i<NUM_STORED_READINGS-1;i++)
+  for(int i = 0; i < NUM_STORED_READINGS-1 ; i++)
   {  
     //break if there is no more VALID readings
     if( readings[i+1].t_reading == 0 )
       break;
-
-    double dw = readings[i].w_reading-readings[i+1].w_reading;
+      
+    // Weight I lost = previous reading - this reading
+    double dw = readings[i+1].w_reading - readings[i].w_reading;
     //Serial.println(dw);
-    double dt = (readings[i].t_reading-readings[i+1].t_reading);
+    
+    //Time elapsed = this - previous
+    unsigned long dt = readings[i].t_reading - readings[i+1].t_reading;
     //Serial.println(dt);
 
-    double curr = dw/dt;
+    // rate of change of weight
+    // multiplying by 1e3 to convert from millis to seconds
+    double curr = 1000 * dw / dt;
     //Serial.print(curr);
-    speed = max(speed, abs(curr) );
+    
+    //note that if curr is negative --> weight increased which doesn't trigger the alarm
+    speed = max(speed, curr );
   }
 
- speed *= 1000;
- Serial.print("max speed: ");
- Serial.println(speed);
+  Serial.print( F("max rate of DECREASE in grams per second! : ") );
+  Serial.println(speed);
 
+  // comparing max. rate to alarm threshold
   if(speed < Weight_Threshold)
       {
-        Serial.println("sub-threshold change ");
+        Serial.println( F("sub-threshold change ") );
         return;
       }
 
-   Serial.println("DANGER !! RINGING ");
-   buzzer_ringing = HIGH;
-   tone (buzzer,3500);  
+  // trigerring buzzer
+  Serial.println( F("DANGER !! RINGING ") );
+  buzzer_ringing = HIGH;
+  tone (buzzer,BUZZ_FREQ);  
 
 }
